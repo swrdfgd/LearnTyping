a9c7561a8dbfb9d771b715b5bdb19fa7c00de25b
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2024-12-22 01:25:19 +0800

    Add files via upload

---
+++ listWord.js
@@ -0,0 +1,3 @@
+prepositions =
+["up","beneath","considering","beyond","about","outside","unlike","down","without","past","save","at","after","versus","anti","until","amid","excluding","excepting","into","towards","in","through","near","among","except","despite","under","on","for","across","upon","around","along","concerning","regarding","besides","with","before","within","opposite","minus","inside","round","off","onto","than","below","as","to","following","since","from","behind","per","over","but","beside","toward","underneath","of","against","aboard","between","above","plus","by","via","like","during"];
+
