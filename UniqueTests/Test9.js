fd7a5930fe4076ebe1d075b9083948a5718136f7
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2025-02-04 11:22:44 +0800

    Add files via upload

--- UniqueTests/Test9.js
+++ UniqueTests/Test9.js
@@ -3,7 +3,7 @@ uniqueTestsTitles["9"] = 'Names';
 
 namaNama = [];
 namaNamaBanyak = 1;
-document.write(document.write('<scr' + 'ipt src="https://cdn.jsdelivr.net/gh/swrdfgd/LearnTyping/UniqueTests/Names/names'+Math.ceil(Math.random()*namaNamaBanyak)+'.js"></scr' + 'ipt>');)
+document.write('<scr' + 'ipt src="https://cdn.jsdelivr.net/gh/swrdfgd/LearnTyping/UniqueTests/Names/names'+Math.ceil(Math.random()*namaNamaBanyak)+'.js"></scr' + 'ipt>')
 
 function genTests9(){
  var hasil = namaNama[Math.floor(Math.random()*namaNama.length)];
