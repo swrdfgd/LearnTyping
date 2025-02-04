5a16c4acad36b66f2f817e0c3833ea1baa2ef6b2
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2025-02-04 11:18:18 +0800

    Add files via upload

---
+++ UniqueTests/Names/names1.js
@@ -0,0 +1,10 @@
+namaNama = namaNama.concat([
+'Bhaadux', 'Vico', 'Berge',
+'Alena', 'Kleopas', 'Bischoffs',
+'Yeong-Ho', 'Cecilie', 'O', 'Maoilsheachlainn',
+'Canaan', 'Aoibhin', 'Tahirovic',
+'Ime', 'Ingunn', 'Lykke',
+'Lukas', 'Lawal', 'Essert',
+'Hidayat', 'Abel', 'Hartell',
+
+])
---
+++ UniqueTests/Test9.js
@@ -0,0 +1,15 @@
+uniqueTests.push(9);
+uniqueTestsTitles["9"] = 'Names';
+
+namaNama = [];
+namaNamaBanyak = 1;
+document.write(document.write('<scr' + 'ipt src="https://cdn.jsdelivr.net/gh/swrdfgd/LearnTyping/UniqueTests/Names/names'+Math.ceil(Math.random()*namaNamaBanyak)+'.js"></scr' + 'ipt>');)
+
+function genTests9(){
+ var hasil = namaNama[Math.floor(Math.random()*namaNama.length)];
+ for (let i = 1; i < 50; i++){
+	hasil += ' ' + namaNama[Math.floor(Math.random()*namaNama.length)];
+ }
+
+ return hasil
+}
