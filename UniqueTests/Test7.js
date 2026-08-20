e3e7e05a86e7cabae513e05e00894c76a2983c38
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2026-08-20 15:07:29 +0800

    Add files via upload

--- UniqueTests/Test7.js
+++ UniqueTests/Test7.js
@@ -10,6 +10,13 @@ function genTests7(){
 	for (let i = 0; i < daftarHurufABC.length; i++){
 		hasil += '  ' + daftarHurufABC[i].repeat(Math.floor(Math.random()*5));
 	}
+	for (let i = 0; i < daftarHurufABC.length; i++){
+		let kataBaru = wordsGen((charListsComplete + 'QWERTASDFGZXCVB;,./').split(''));
+		while (kataBaru[0].toLowerCase() != daftarHurufABC[i]){
+			kataBaru = wordsGen((charListsComplete + 'QWERTASDFGZXCVB;,./').split(''));
+		}
+		hasil += '  ' + kataBaru;
+	}
 	hasil += ' ' + daftarHurufABC;
 	return hasil
 }
